1bbd13f048f63f8b80f65764b505df3659e0ffc2
Adrian Popa <adrian.popa.box@gmail.com>
2020-10-26 12:29:46 +0100

    a* simplified or rather mutated
    a* simplified or rather mutated

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -5,127 +5,12 @@
  * Assignment: Collinear, Grade 100/100, https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/specification.php
  */
 
-import java.util.HashMap;
-
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 
-    private class SNode implements Comparable<SNode> {
-        // the root node
-        final SNode previous;
-        // the current board
-        final Board board;
-        // the number of moves to reach the current board
-        final int moves;
-
-        public SNode(Board board, SNode previous) {
-            this.previous = previous;
-            this.board = board;
-            this.moves = previous == null ? 0 : previous.moves + 1;
-        }
-
-        public int fcost() {
-            return board.manhattan() + moves;
-        }
-
-        @Override
-        public int compareTo(SNode o) {
-            if (this.fcost() == o.fcost())
-                return 0;
-            else if (this.fcost() > o.fcost())
-                return 1;
-            else
-                return -1;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            {
-                sb.append(String.format("manhattan = %d ", board.manhattan()));
-                sb.append(String.format("moves = %d ", moves));
-                sb.append(String.format("priority = %d\n", fcost()));
-                sb.append(board.toString());
-            }
-
-            return sb.toString();
-        }
-    }
-
-    private SNode solve(Board initial) {
-        final MinPQ<SNode> openSet = new MinPQ<>(/* initial.dimension() * initial.dimension() */);
-        final HashMap<Board, SNode> _openSet = new HashMap<Board, SNode>();
-        final HashMap<Board, SNode> closedSet = new HashMap<Board, SNode>();
-
-        // add the start node to open set
-        SNode node = new SNode(initial, null);
-        openSet.insert(node);
-        _openSet.put(initial, node);
-
-        int steps = 0;
-        // loop
-        while (!openSet.isEmpty()) {
-            steps++;
-            
-            // get the best candidate from neighbors,
-            // which is the node in open with lowest cost
-            // and
-            // remove the node from open set
-            SNode current = openSet.delMin();
-            _openSet.remove(current.board);
-            StdOut.println("current: \n" + current);
-
-            // add current to closed set
-            closedSet.put(current.board, current);
-
-            // is current the target node?
-            if (current.board.isGoal()) {
-                // is solved => reconstruct the path
-                StdOut.println("STEPS: " + steps);
-                return current;
-            }
-            if (current.board.hamming() == 2 && current.board.twin().isGoal()) {
-                return null;
-            }
-
-            // for each neighbor of current node
-            for (Board neighbor : current.board.neighbors()) {
-                // if not traversable or already in closed set skip it
-                if (closedSet.containsKey(neighbor)) {
-                    continue;
-                }
-
-                // 1. distance from starting/initial node
-                int gcost = current.moves + 1; 
-                // 2. distance to target node
-                int hcost = neighbor.manhattan();
-                // 3. fcost = hcost + gcost        
-                int fcost = hcost + gcost;
-
-                // if new path to neighbor is shorter OR neighbor is not in open set
-                boolean isInOpenSet = _openSet.containsKey(neighbor);
-                if (current.fcost() < fcost || !isInOpenSet) {
-                    // set new cost of neighbor
-                    // and
-                    // set parent of neighbor to current
-                    SNode nnode = new SNode(neighbor, current);
-                    StdOut.println("neighbor: \n" + nnode);
-
-                    if (!isInOpenSet) {
-                        openSet.insert(nnode);
-                        _openSet.put(neighbor, nnode);
-                    }
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private SNode solution = null;
+    private final SearchNode solution;
 
     /**
      * Find a solution to the initial board (using the A* algorithm)
@@ -133,6 +18,10 @@ public class Solver {
      * @param initial the starting board
      */
     public Solver(Board initial) {
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
+
         this.solution = solve(initial);
     }
 
@@ -140,14 +29,18 @@ public class Solver {
      * @return true if a solution is found, false otherwise
      */
     public boolean isSolvable() {
-        return this.solution != null;
+        return this.solution.isTwin == false;
     }
 
     /**
      * @return min number of moves to solve initial board; -1 if unsolvable
      */
     public int moves() {
-        return isSolvable() ? solution.moves : -1;
+        if (isSolvable()) {
+            return solution.moves;
+        }
+
+        return -1;
     }
 
     /**
@@ -159,13 +52,103 @@ public class Solver {
         }
 
         Stack<Board> queue = new Stack<>();
-        SNode node = solution;
+        SearchNode node = solution;
         while (node != null) {
             queue.push(node.board);
             node = node.previous;
         }
-
         return queue;
+    }
+
+    private SearchNode solve(Board initial) {
+        final MinPQ<SearchNode> openList = new MinPQ<>();
+
+        // add the start node to open set
+        SearchNode start = new SearchNode(initial, null, false);
+        openList.insert(start);
+        SearchNode startTwin = new SearchNode(initial.twin(), null, true);
+        openList.insert(startTwin);
+
+        // loop
+        while (!openList.isEmpty()) {
+            SearchNode current = openList.delMin();
+
+            // is current the target node?
+            if (current.board.isGoal()) {
+                return current;
+            }
+
+            // for each neighbor of current node
+            for (Board board : current.board.neighbors()) {
+                SearchNode neighbor = new SearchNode(board, current);
+                if (current.previous == null || //
+                        !current.previous.board.equals(board)) {
+                    openList.insert(neighbor);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private final SearchNode previous;
+        private final Board board;
+        // moves represents the g(n) function cost
+        // how far away the node is from initial node
+        private final int moves;
+        private final boolean isTwin;
+
+        private int fcost = Integer.MIN_VALUE;
+        private int hcost = Integer.MIN_VALUE;
+        
+        public SearchNode(Board board, SearchNode previous, boolean isTwin) {
+            this.previous = previous;
+            this.board = board;
+            if (previous == null) {
+                this.moves = 0;
+            } else {
+                this.moves = previous.moves + 1;
+            }
+            this.isTwin = isTwin;
+        }
+        
+        public SearchNode(Board board, SearchNode previous) {
+            this(board, previous, previous.isTwin);
+        }
+
+        private int hcost() {
+            if (this.hcost == Integer.MIN_VALUE) {
+                this.hcost = board.manhattan();
+            }
+
+            return this.hcost;
+        }
+
+        private int fcost() {
+            if (this.fcost == Integer.MIN_VALUE) {
+                this.fcost = hcost() + moves;
+            }
+
+            return this.fcost;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            int costDiff = this.fcost() - that.fcost();
+            return costDiff == 0 ? this.hcost() - that.hcost() : costDiff;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("manhattan = %d ", board.manhattan()));
+            sb.append(String.format("moves = %d ", moves));
+            sb.append(String.format("priority = %d\n", fcost()));
+            sb.append(board.toString());
+
+            return sb.toString();
+        }
     }
 
     // test client (see below)
